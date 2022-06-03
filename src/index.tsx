4566ff7c3144929475682cd476ba6137b755a06c
jezerdave <jezerdavebacquian@gmail.com>
2022-06-03 15:38:17 +0800

    update import

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {AuthProvider} from "./context/auth/AuthProvider";
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en.json'
+
+TimeAgo.addDefaultLocale(en)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
